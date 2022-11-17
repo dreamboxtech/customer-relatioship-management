7ff16384ef9cdfe0ffd14feb481b3e8f272edde7
dreamboxtech <bbbolaleye@gmail.com>
2022-11-17 20:50:23 +0100

    test settings

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from leads.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello)
 ]
+
+
+
+
+
+
--- leads/admin.py
+++ leads/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from .models import Agent, User, Lead
 
 # Register your models here.
-admin.register()
+admin.site.register(Lead)
+admin.site.register(User)
+admin.site.register(Agent)
+ 
